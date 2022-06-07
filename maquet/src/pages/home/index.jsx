bceaacd4c02702c783613d42e68bde72f9b479a4
Vict Mart <codigovictor01@gmail.com>
2022-06-07 18:26:13 +0200

    uptdate footer

--- maquet/src/pages/home/index.jsx
+++ maquet/src/pages/home/index.jsx
@@ -4,66 +4,55 @@ import search from '../../assets/search.svg'
 import location from '../../assets/location.svg'
 import union from '../../assets/union.svg'
 import PlantCard from '../../components/plantCard'
-<<<<<<< HEAD
 import WeatherInfo from '../../components/weatherInfo';
 import Footer from '../../components/footer';
-=======
-import CurrentWeatherInfo from '../../components/CurrentWeatherInfo';
-import WeatherInfoByName from '../../components/WeatherInfoByName'
-import { useCurrentLocation, useCityWeatherByName} from '../../data/weather.hooks.js';
-import { useState } from 'react';
->>>>>>> 0be8800f0887dd1cfa8b702624be55a160c5c83b
 
 function Home() {
-    const [inputValue, updateInputValue] = useState('')
-    const {currentLocation, arrPlantsFiltered, filterPlantByName} = useCurrentLocation();
-    const {cityWeatherByName, getCityByName} = useCityWeatherByName();
-    const [isCurrentLocation, updateIsCurrentLocation] = useState(true)
-    console.log(currentLocation)
-    
-    const showWeatherByName = () => {
-        if(inputValue.length >=1){
-            getCityByName(inputValue);
-            updateIsCurrentLocation(false);
-            updateInputValue('');
-        }else{
-            console.log('Input vacío, no se puede mostrar ninguna ciudad');
-        }
-        
-    }
-
     return (
         <>
             <Header></Header>
-            
-            <div className='input__container'>
-                <h1 className='welcome-title'>Bienvenido a tu zona cuidado</h1>
-                <div className='form__container'>
-                    <input onChange={e => updateInputValue(e.target.value)} value={inputValue} className='input-box' placeholder='Localización'></input>
-                    <button onClick={showWeatherByName}><img src={search}></img></button>
-                    <button onClick={() => updateIsCurrentLocation(true)}><img src={location}></img></button>
+            {/* <Container>
+                <Row>
+                    <Col> */}
+                    <div className='input__container'>
+                    <h1 className='welcome-title'>Bienvenido a tu zona cuidado</h1>
+                    <div className='form__container'>
+                        <input className='input-box' placeholder='Localización'></input>
+                        <button><img src={search}></img></button>
+                        <button><img src={location}></img></button>
+                    </div>
                 </div>
-            </div>
-        
-            {isCurrentLocation ? <CurrentWeatherInfo currentLocationWeather={currentLocation}></CurrentWeatherInfo> : <WeatherInfoByName cityWeatherByName={cityWeatherByName}></WeatherInfoByName>}
-           
-            <section className='plants-section'>
+                    {/* </Col>
+                </Row>
+            </Container> */}
+            <section>
+
+            <WeatherInfo></WeatherInfo>
+
+                {/* <div className='img__container'>
+                    <div className='blur__container'>
+
+                        <div className='info-weather__container'></div>
+                    </div>
+                </div> */}
+              
                 <div className='cards-section'>
-                     <h1 className='plants-title' > Tus plantas</h1>
+
+                    <h1 className='plants-title' > Tus plantas</h1>
+
                     <div className='input-plants__container'>
                         <img className='union-icon' src={union} alt="" />
-                        <input className='input-plants' placeholder='Busca tus plantas' onChange={filterPlantByName}></input>
+                        <input className='input-plants' placeholder='Busca tus plantas'></input>
                     </div>
+
                     <section className='cards__container'>
-                    {arrPlantsFiltered?.map((c,i) => <PlantCard key={i} card={c} currentWeather={isCurrentLocation ? currentLocation : cityWeatherByName}></PlantCard>)}
+                    <PlantCard></PlantCard>
                     </section>
                 </div>
-<<<<<<< HEAD
-                <Footer></Footer>
+                
                
-=======
->>>>>>> 0be8800f0887dd1cfa8b702624be55a160c5c83b
             </section>
+            <Footer></Footer>
         </>
     )
 }
